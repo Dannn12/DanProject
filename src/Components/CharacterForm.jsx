3832ca85ae984e9ce2e2b063f2a4745850d9110b
Dannn12 <daniel.murden@icloud.com>
2022-10-25 14:58:26 +0100

    FrontEnd basic

---
+++ src/Components/CharacterForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ChaForm = ({props}) => {
+<div>
+    <form>
+    <h1>Name:</h1>
+
+    <h2>Age:</h2>
+
+    <h2>Ability:</h2>
+
+    <h2>Interesting Fact:</h2>
+    </form>
+</div>
+
+}
